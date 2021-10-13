1bc5596a6b53165bf138c396b7ee00e8a450b8e2
vientran199 <vientran1999@gmail.com>
2021-10-13 21:11:01 +0700

    fix warning title

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,12 +1,18 @@
 import type { AppProps } from "next/app"
 import { Box } from "@chakra-ui/react"
+import Head from 'next/head'
 import Provider from "@components/shared/Provider"
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<Provider>
-			<Component {...pageProps} />
-		</Provider>
+		<>
+			<Head>
+				<title>Chain Store</title>
+			</Head>
+			<Provider>
+				<Component {...pageProps} />
+			</Provider>
+		</>
 	)
 }
 export default MyApp
--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -1,13 +1,10 @@
 // @next/next/no-document-import-in-page
-import Document, { Html, Head, Main, NextScript } from "next/document"
+import Document, { Html, Main, NextScript } from "next/document"
 
 class MyDocument extends Document {
 	render() {
 		return (
 			<Html lang="en">
-				<Head>
-					<title>Chain Store</title>
-				</Head>
 				<body>
 					<Main />
 					<NextScript />
